7323aeff13b807fc18474287c5884190f7c73dce
Somtochukwu Nkem <sewkito@gmail.com>
2025-03-15 08:36:04 +0100

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,43 +8,43 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: BASE_URL,
       lastModified: new Date(),
       changeFrequency: "yearly",
-      priority: 1,
+      priority: 1
     },
     {
       url: `${BASE_URL}/who-we-are`,
       lastModified: new Date(),
       changeFrequency: "yearly",
-      priority: 2,
+      priority: 0.8
     },
     {
       url: `${BASE_URL}/events`,
       lastModified: new Date(),
       changeFrequency: "monthly",
-      priority: 3,
+      priority: 0.6
     },
     {
       url: `${BASE_URL}/blogs`,
       lastModified: new Date(),
       changeFrequency: "monthly",
-      priority: 4,
+      priority: 0.9
     },
     {
       url: `${BASE_URL}/support-us`,
       lastModified: new Date(),
       changeFrequency: "yearly",
-      priority: 5,
+      priority: 0.4
     },
     {
       url: `${BASE_URL}/donate`,
       lastModified: new Date(),
       changeFrequency: "yearly",
-      priority: 6,
+      priority: 0.3
     },
     {
       url: `${BASE_URL}/contact`,
       lastModified: new Date(),
       changeFrequency: "yearly",
-      priority: 7,
+      priority: 0.2
     },
   ];
 }
