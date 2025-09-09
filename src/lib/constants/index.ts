98b96dab3854fc5dc44c59713231afb9a0cd9d5f
Somtochukwu Nkem <sewkito@gmail.com>
2025-09-09 12:08:35 +0100

    Update index.ts

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -2,5 +2,5 @@ export const BASE_URL = "https://www.zitaonyekafoundation.org";
 
 export const API_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://www.admin.zitaonyekafoundation/api"
+    ? "https://admin.zitaonyekafoundation.org/api"
     : "http://localhost:3000/api";
